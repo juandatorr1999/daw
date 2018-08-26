bfca6907673305260b1d71975f955a5e0edeb2df
3t4n <A01209675@itesm.mx>
2018-08-25 20:40:35 -0500

    Validador de contraseñas agregado

---
+++ laboratorio5/validar.js
@@ -0,0 +1,34 @@
+let password = function(){
+	let color = "red";
+	let msg = "Mala seguridad usa carácteres alpanúmericos y especiales";
+	var input = String(document.getElementById("pass1").value);
+	seguridad = 0;
+	let array = ["[a-z]","[A-Z]","[0-9]","[#|%|&|!|=|+|-|.|:|;|,]"];
+	let a = 1;
+	let bar=0;
+	for(a;a<=4;a++){
+		var regex = new RegExp(array[a-1],'g');
+		let res = input.match(regex);
+		if(res)
+			bar = bar + res.length*a*a
+	}
+	if(bar>25 && bar<75){
+		color = "orange";
+		msg = "Ya casi";}
+	else if(bar>75){
+		color = "green";
+		msg = "Excelente :)";}
+	document.getElementById("bar").style="width:"+bar+"%;background-color:"+color+";";
+	document.getElementById("msg").innerHTML=msg;
+	password2();
+};
+
+let password2 = function(){
+	if(document.getElementById("pass1").value===document.getElementById("pass2").value){
+		document.getElementById("msg2").innerHTML="Coinciden las contraseñas :)";
+	}
+	else{
+		document.getElementById("msg2").innerHTML="No coinciden las contraseñas :(";
+	}
+}
+
