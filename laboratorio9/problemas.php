5ac7b70c96adcd4b25594b62d7f548957c8dae4b
Etan <a01209675@itesm.mx>
2018-09-08 17:47:29 -0500

    Formas HTML y Funciones php

--- laboratorio9/problemas.php
+++ laboratorio9/problemas.php
@@ -1,6 +1,5 @@
 <?php
 	$funcion = $_GET['funcion'];
-	$arreglo = $_GET['arreglo'];
 	$dato = $_GET['dato'];
 
 	function promedio($data)
@@ -12,25 +11,25 @@
 			$resultado += $value;
 		}
 		$resultado = $resultado/count($arreglo);
+		return $resultado;
 	}
 
 	function mediana($data)
 	{
 		$arreglo = explode(',',$data);
 		sort($arreglo,SORT_NUMERIC);
-		if(count($arreglo))%2==1)
-			$res = $arreglo[ceil(count($arreglo)/2)];
+		if(count($arreglo)%2==1)
+			$res = $arreglo[ceil(count($arreglo)/2-1)];
 		else{
 			$indice = count($arreglo)/2;
-			$res = ($arreglo[$indice]+$arreglo[$indice+1])/2
+			$res = ($arreglo[$indice]+$arreglo[$indice+1])/2;
 		}
 			return $res;
+	} 
 
-
-	}
 	function pmo($data)//promedio media ordenado
 	{
-		$res=$data."<ul><li>Promedio: ".promedio($data)."</li><li>Mediana: ".mediana."</li><li>";
+		$res=$data."<ul><li>Promedio: ".promedio($data)."</li><li>Mediana: ".mediana($data)."</li><li>";
 		$arreglo = explode(',',$data);
 		sort($arreglo,SORT_NUMERIC);
 		$res .= join(',',$arreglo)."</li><li>";
@@ -42,10 +41,27 @@
 	{
 		$res="<table>";
 		foreach (range(1,$num) as $n){
-			$res .= "<tr><td>".pow($num,2)."</td><td>".pow($num,3)."</td>"
+			$res .= "<tr><td>".pow($n,2)."</td><td>".pow($n,3)."</td>";
 		}
 		$res.="</table>";
 		return $res;
 	}
+
+	function fibo($num)
+	{
+		if($num==1 || $num==0)
+			return 1;
+		return fibo($num-1)+fibo($num-2);
+	}
+	if($_GET['funcion']==="promedio")
+		echo promedio($_GET['dato']);	
+	if($_GET['funcion']==="mediana")
+		echo mediana($_GET['dato']);	
+	if($_GET['funcion']==="pmo")
+		echo pmo($_GET['dato']);	
+	if($_GET['funcion']==="cc")
+		echo cc($_GET['dato']);	
+	if($_GET['funcion']==="fibo")
+		echo fibo($_GET['dato']);
 
 ?>
